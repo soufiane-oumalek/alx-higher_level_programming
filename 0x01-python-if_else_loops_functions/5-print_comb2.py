2d0299192c55ace3211b0b6d0f7e79e0d656197a
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-06 17:52:44 -0700

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    print("{:02d}".format(num), end=", ")
-    if num == 99:
-        print('\n')
+    if num < 99:
+        print("{:02d}".format(num), end=", ")
+    else :
+        print(99)
