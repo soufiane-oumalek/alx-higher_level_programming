ce5b960a7c333861398c15e567f8937fed51fe43
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-07 18:25:06 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBUzz", end=" ")
-        if i % 3 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
