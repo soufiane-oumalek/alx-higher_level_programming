07caf9694f067ac9f6636154dfebf369fa01988b
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-08 17:15:20 -0700

    hidden 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     name_m = dir(hidden_4)
     for name in name_m:
-        if name [:2] != "__":
+        if name[:2] != "__":
             print(name)
