c3a93fada75d7d74980afdb6904cb87430648a1d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-12 08:27:46 -0700

    modify list without using origin list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,10 @@ def new_in_list(my_list, idx, element):
         if idx < 0 or idx >= len(my_list):
             return my_list
         else:
-            new_list = my_list
-            new_list[idx] = element
-            return my_list
+            new_list = []
+            for i, item in enumerate(my_list):
+                if i == idx:
+                    new_list.append(element)
+                else:
+                    new_list.append(item)
+            return new_list
