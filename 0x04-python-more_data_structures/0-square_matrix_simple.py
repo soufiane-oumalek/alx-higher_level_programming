88b72836ef819854ce8b23783f1e148de72f7fe6
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-14 11:48:55 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/pyhton3
 def square_matrix_simple(matrix=[]):
-    def square_matrix_simple(matrix=[]):
-    matrix_copy = [row[:] for row in matrix]
-    for i in range(len(matrix_copy)):
-        for j in range(len(matrix_copy[i])):
-            matrix_copy[i][j] = matrix_copy[i][j] ** 2
-    return matrix_copy)
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
