7373653aa97bc56542460b6b9624dfb9e4190ac4
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-27 13:11:39 -0700

    tsk_2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """declare class Square"""
+    def __init__(self, size=0):
+        """intialize square
+        Args:
+            size(int): the size of square"""
+
+        if(type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif(size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
