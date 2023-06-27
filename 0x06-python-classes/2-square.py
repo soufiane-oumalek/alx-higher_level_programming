ef261c68818435ccb7de85649031aec5455b9ef2
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-27 13:23:03 -0700

    tsk_2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         Args:
             size(int): the size of square"""
 
-        if(type(size) is not int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif(size < 0):
             raise ValueError("size must be >= 0")
