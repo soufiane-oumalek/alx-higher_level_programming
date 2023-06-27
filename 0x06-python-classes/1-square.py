c2e5b3146d54a3d91b24364eb24c6c818dc402bf
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-27 12:14:47 -0700

    tsk 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """declare class"""
+    def __init__(self, size):
+
+        """intialize square
+
+
+        args:
+            size(int): the size of square:"""
+        self.__size = size
