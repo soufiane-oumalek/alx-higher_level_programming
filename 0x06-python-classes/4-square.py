223d53129163ea24629a8d86cddb56daa4c88381
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-27 19:31:30 -0700

    tsk_5 tsk_4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """declare class square"""
+
+    def __init__(self, size=0):
+        """intialize square
+        Args:
+
+        size (int): size of square"""
+        self.size = size
+
+        def area(self):
+            return (self.__size ** 2)
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if type(value) is not int:
+                raise TypeError("size ust be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """declare class square"""
+
+    def __init__(self, size=0):
+        """intialize square
+        Args:
+
+        size (int): size of square"""
+        self.size = size
+
+        def area(self):
+            return (self.__size ** 2)
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if type(value) is not int:
+                raise TypeError("size ust be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+        def my_print(self):
+            if not self.__size:
+                print("")
+            for i in range(self.__size):
+                print("#" * self.__size)
