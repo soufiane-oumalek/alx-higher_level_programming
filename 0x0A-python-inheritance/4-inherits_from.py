d48c3a8539065c958e10eb8a34cd6f8065772d32
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-10 18:47:29 +0000

    tsk_4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines class"""
+
+
+def inherits_from(obj, a_class):
+    """function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+    Args:
+    @obj: is an object
+    @a_class: is a class
+    """
+    if is issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
