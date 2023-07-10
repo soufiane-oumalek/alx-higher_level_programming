1193d3395e63341599a6734bc7a07eed54d32cfd
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-10 20:32:39 +0000

    tsk_11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines rectangle class square"""
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(Rectangle):
@@ -14,3 +15,9 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+    
+    def area(self):
+        return self.__size ** 2
