0b91cb2d026d988f72906bb0289a1d207817dd56
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-11 19:53:26 +0000

    tsk_9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,17 +4,17 @@ class Student:
     """declare class student"""
 
 
-def __init__(self, first_name, last_name, age):
-    """intiliaze a student
-    Args:
-    @first_name: the first name of student
-    @last_name: the last name of student
-    @age: the age of student
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        """intiliaze a student
+        Args:
+        @first_name: the first name of student
+        @last_name: the last name of student
+        @age: the age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 
-def to_json(self):
-    return self.__dict__
+    def to_json(self):
+        return self.__dict__
