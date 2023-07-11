457f04ba04047016427d7299d7b525805ef59cfa
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-11 20:21:47 +0000

    tsk_10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Class Student to JSON"""
+
+
+class Student:
+    """declare class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """intiliaze a student
+        Args:
+        @first_name: the first name of student
+        @last_name: the last name of student
+        @age: the age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a Student
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
