65741b4eb51c62937a9f59777552c4fd87835ed3
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-11 21:28:17 +0000

    tsk_11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Class Student to JSON"""
+
+
+class Student:
+    """declare class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """intiliaze a student
+        Args:
+        @first_name: the first name of student
+        @last_name: the last name of student
+        @age: the age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a Student
+        """
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for i in attrs:
+            try:
+                dic[i] = self.__dict__[i]
+            except FileNotFoundError:
+                pass
+            return dic
+
+    def reload_from_json(self, json):
+        """retrieves a dictionary representation of a Student"""
+
+        for k in json:
+            try:
+                setattr(self, key, json[key])
+            except FileNotFoundError:
+                pass
