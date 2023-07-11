75e98275de873633c81c92ea9bd25b71334b5364
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-11 19:24:46 +0000

    tsk_8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,7 +3,7 @@
 
 
 def class_to_json(obj):
-     """function that returns the dictionary
-     description with simple data structure
+    """returns the dictionary
+    description with simple data structure
     """
     return obj.__dict__
