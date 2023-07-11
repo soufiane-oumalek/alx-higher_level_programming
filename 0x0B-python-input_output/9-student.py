7d5402f2d6b9fb4df62b6fc59a894fdf64fa75b0
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-11 20:00:16 +0000

    tsk_9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Class Student to JSON"""
+
+
 class Student:
     """declare class student"""
-
 
     def __init__(self, first_name, last_name, age):
         """intiliaze a student
@@ -14,7 +15,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         return self.__dict__
