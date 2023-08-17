05c7a9b6a212c94aa199f82e78347c6bf2242f24
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-17 03:36:51 +0000

    tasks.*SQL

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT ts.title FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+ON ts.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
