bfa0b33af49d204daa29da7a44816fe17eb0fff8
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-17 03:00:54 +0000

    tasks.*SQL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tvshows.title, tv_show_genres.genres_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show.id ORDER BY tv_shows.title ASC, tv_show_genres.genres_id ASC;
+SELECT tv_shows.title, tv_show_genres.genres_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show.id ORDER BY tv_shows.title ASC, tv_show_genres.genres_id ASC;
