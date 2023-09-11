442ab76170dac01491ca0b85972759fc0ee2a54a
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-11 16:58:38 +0000

    javaScript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Var1 = "C is fun";
-const Var2 = "Python is cool";
-const Var3 = "JavaScript is amazing";
-console.logo(Var1);
-console.logo(Var2);
-console.logo(Var3);
+const Var1 = 'C is fun';
+const Var2 = 'Python is cool';
+const Var3 = 'JavaScript is amazing';
+console.log(Var1);
+console.log(Var2);
+console.log(Var3);
