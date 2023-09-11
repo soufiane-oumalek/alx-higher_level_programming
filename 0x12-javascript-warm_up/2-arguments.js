3341e5642168a495393fdfbcff72e275dd13056c
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-11 17:01:26 +0000

    javaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@
 const argv = process.argv;
 if (argv.length === 2) {console.log("No argument");}
 else if (argv.length === 3) {console.log("Argument found");}
-else {console.logo("Arguments found");}
+else {console.log("Arguments found");}
