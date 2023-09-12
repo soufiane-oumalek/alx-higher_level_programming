d77486c017d472849d6abfefc8fc6919ef1430cb
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-12 20:31:04 +0000

    update 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,9 +15,9 @@ class Rectangle {
   }
 
   rotate () {
-    const temp = this.height;
+    const tmp = this.width;
     this.width = this.height;
-    this.height = temp;
+    this.height = tmp;
   }
 
   double () {
