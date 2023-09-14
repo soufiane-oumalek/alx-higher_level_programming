616889aee577dc98005bd4e1b927a1b70271dcf6
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-14 02:00:05 +0000

    javaScript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((x, i) => x * i));
+const mList = require('./100-data').list;
+console.log(mList);
+console.log(mList.map((c, i) => c * i));
