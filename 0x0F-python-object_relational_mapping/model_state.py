a7ed6b58359df383fbcc4f336e3445ba99b26b8d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-17 18:22:33 +0000

    update 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """states model"""
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(Base):
