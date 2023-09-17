ad21ebe74a46f2fb905b924b57de9ae049d6060d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-17 20:30:50 +0000

    advenced

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -9,6 +9,6 @@ class City(Base):
     """ class class """
     __tablename__ = 'cities'
 
-    id = Column(Integer, unique=True primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """relationshipstate"""
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
