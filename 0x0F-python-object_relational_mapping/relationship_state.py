873645947dea44ae5ddc3a824ebe6d7839db0c7e
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-17 20:11:45 +0000

    advenced

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -11,6 +11,6 @@ class State(Base):
     """ class class """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade="all, delete", backref='state')
