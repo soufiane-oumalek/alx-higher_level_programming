8d684d7fc210417e2bf7d6419f7fe18efa865e7f
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-17 17:20:54 +0000

    update 6

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         sys.argv[1],
         sys.argv[2],
         sys.argv[3]),
-        pool_pre_ping=True)
+        pool_pre_ping=True,)
     Base.metadata.create_all(engine)
