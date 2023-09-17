8d396e1a8027f4414177746a7587c03ec30fb892
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-17 18:31:17 +0000

    update 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class State(Base):
     """ define city class """
