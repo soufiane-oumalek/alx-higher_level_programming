c6a777ccd4e1cf00877678294ab50a366ca1d76c
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-17 19:58:54 +0000

    advenced

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """relationshipcity"""
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
+from relationship_state import Base
 
 
 class City(Base):
