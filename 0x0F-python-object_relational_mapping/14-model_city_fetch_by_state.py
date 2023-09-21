bffbbec836923e88533eb27d318f14452fff2fd7
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-21 19:25:48 +0000

    update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,10 +14,12 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     ses = Session()
     ob = ses.query(City, State)\
         .join(State, State.id == City.state_id).all()
+
     if ob:
         for city, state in ob:
             print("{}: ({}) {}".format(state.name, city.id, city.name))
