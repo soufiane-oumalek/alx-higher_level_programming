f1127677d1a0adb0a782d9313cead559f327a34f
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-28 17:33:29 +0000

    1 to 5

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,4 @@
+#!/usr/bash
+# takes in a URL as an argument, sends a GET request to the URL,
+# and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/usr/bash
+# takes in a URL, sends a POST request to the passed URL,
+# and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
