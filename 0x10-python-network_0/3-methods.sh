6c60e696e94c6457b0110972731882aebf52fa5d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-28 18:15:06 +0000

    update 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/usr/bash
-# takes in a URL and displays all HTTP methods the server will accept.
+# takes in a URL and displays all HTTP methods the server will accept
 curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
