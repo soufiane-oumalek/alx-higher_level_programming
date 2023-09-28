188fea51393895e00d201abe58dc0e5815362d86
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-28 18:04:11 +0000

    update 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl -sI $1 | grep -i content-length | awk '{print $2}'
+# A Bash script that takes in a URL, sends a request and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
