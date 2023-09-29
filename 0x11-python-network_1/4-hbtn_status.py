268c8b7d73487f8159aa3d660e98d1bbf2f5a3c0
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-29 16:55:30 +0000

    fetches

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
