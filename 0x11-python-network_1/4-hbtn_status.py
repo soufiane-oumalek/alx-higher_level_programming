f8dd87ac9c8b0115ca1b60365e973a11ae4569a9
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-09-29 18:37:03 +0000

    fetches

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = ('https://intranet.hbtn.io/status')
     result = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format (type(result.text)))
+    print("\t- type: {}".format(type(result.text)))
     print("\t- content: {}".format(result.text))
