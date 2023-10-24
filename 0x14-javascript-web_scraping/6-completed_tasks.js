96749c9b3b0fc2d7d929409d2488d5e4716553dc
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-24 17:06:44 +0000

    tasks completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = process.argv[2];
-
-request(url, { json: true }, (err, res, body) => {
-  if (err) console.log(err);
-  const results = {};
-
-  for (const task of body) {
-    if (task.completed) {
-        results[task.userId] = (results[task.userId] || 0) + 1;
+request(url, function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-    console.log(results);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    });
+    console.log(completed);
+  }
+});
