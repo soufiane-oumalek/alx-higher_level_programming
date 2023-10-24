8dd55a26de8c76d3c4a238e1e58ecfe878c5f701
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-24 15:40:46 +0000

    display_status-code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request-promise');
+const request = require('request');
 const url = process.argv[2];
 request.get(url).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
