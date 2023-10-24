ebe12ae7cd633588762a6237626ec88076906bca
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-24 16:33:10 +0000

    prints the number

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const api_url = process.argv[2]
+const api_url = process.argv[2];
 request(api_url, function (error, response, body) {
   if (!error) {
     const rsult = JSON.parse(body).results;
