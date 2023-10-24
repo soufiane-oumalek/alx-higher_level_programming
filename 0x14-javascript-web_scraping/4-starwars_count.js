0ea816d18852f88f37f1b4c5740cead3c67decbe
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-24 16:38:01 +0000

    prints the number

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const api_url = process.argv[2];
-request(api_url, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const rsult = JSON.parse(body).results;
-    console.log(rsult.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
